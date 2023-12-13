0966f48dc47dc3d02419a88121d15bca843f23bc
Regulus <104451548+Bragandos@users.noreply.github.com>
2023-12-13 19:53:07 +0200

    contact
    final

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="contacts">
-    <h1>This is a contacts page</h1>
+  <div class="home">
+    <div class="addpagebody">
+      <div class="post">
+
+        
+        <div class="postbody2">
+          <div>
+            <img id="jonny" src="../assets/jonny.jpg" alt="">
+          </div>
+          <div class="conSpan">
+            <span class="date"><b>Head of Staff</b></span>
+            <span class="body">Jon Ehy</span>
+            <span class="body">+44-11-7325-7425</span>
+            <span class="body">6, Via Montebello</span>
+            <span class="body">40121 Bologna</span>
+            <span class="body">BO Italy</span>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
   </div>
 </template>
 
